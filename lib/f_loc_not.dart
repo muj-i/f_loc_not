bc32a1504920dd79d235d81224749df8e8d3e8d4
muj-i <dev.muj.i@icloud.com>
2024-09-16 13:00:24 +0600

    flutter local notification service added

--- lib/f_loc_not.dart
+++ lib/f_loc_not.dart
@@ -1,8 +1,17 @@
+import 'package:f_loc_not/src/f_loc_not_services.dart';
 
 import 'f_loc_not_platform_interface.dart';
 
 class FLocNot {
   Future<String?> getPlatformVersion() {
     return FLocNotPlatform.instance.getPlatformVersion();
+  }
+
+  static Future<void> init() async {
+    return FLocNotServices.init();
+  }
+
+  static Future<void> showLocalNotification(String title, String body) async {
+    return FLocNotServices.showLocalNotification(title, body);
   }
 }
